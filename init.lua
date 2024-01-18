c2683713560df3e09733116f5b695071cc2e4b9b
bearded-tomato <73724848+bearded-tomato@users.noreply.github.com>
2024-01-18 20:42:26 +1100

    updating desc

--- init.lua
+++ init.lua
@@ -333,8 +333,8 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
--- setting keybinding for fugitive
-vim.api.nvim_set_keymap('n', '<Leader>gs', ':G<CR>', { desc = '[S]tatus with vim fugitive', noremap = true, silent = true })
+-- setting keybinding for fugitive git status
+vim.api.nvim_set_keymap('n', '<Leader>gs', ':Git<CR>', { desc = '[S]tatus with vim fugitive', noremap = true, silent = true })
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
